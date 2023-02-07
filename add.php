57f35bcdef9dd754ee812f6432c866d67724e478
Tahmina6023 <tahminatamanna799@gmail.com>
2023-02-07 21:13:40 +0600

    commit with index and add page

--- add.php
+++ add.php
@@ -1,30 +1,4 @@
-<?php
-    include("connection.php");
-    $msg ="";
-    
 
-        if(isset($_POST['submit'])){
-         $p_name        = $_POST['pName'];
-         $p_price       = $_POST['pPrice'];
-         
-         $image_name  = $_FILES['image']['name']; 
-         $temp_file   = $_FILES['image']['temp_file'];
-         $up_loc      = 'img/'.$image_name; 
-
-
-        $sql = "INSERT INTO product(product_name,product_price,product_image)VALUES('$p_name','$p_price','$image_name')";
-
-        if(mysqli_query($con,$sql) == TRUE){
-            move_uploaded_file($temp_file,$up_loc);
-            $msg="Products added to database successfully!😃";
-        }
-        else{
-            $msg = "Failed to add the product!☹️";
-        }
-    }
-
-        
-    ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -47,7 +21,7 @@
             <div class="row d-flex justify-content-center">
                 <div class="col-md-6 bg-light mt-5 rounded">
                     <h2 class="text-center p-2">Add Product Information</h2>
-                    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" class="p-2" enctypr="multipart/form-data">
+                    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" class="p-2" enctype="multipart/form-data">
                         <div class="form-group mb-2">
                             <input type="text" name="pName" class="form-control" placeholder="Product Name" required>
                         </div>
@@ -57,13 +31,42 @@
                         <div class="form-group mb-4">
                         <div class="custom-file">
                             <input type="file" name="image" accept=".jpg, .jpeg, .png" class="custom-file-input" id="customFile" required>
+                        
+                        <?php
+    include("connection.php");
+    $msg ="";
+    
+
+        if(isset($_POST['submit'])){
+         $p_name        = $_POST['pName'];
+         $p_price       = $_POST['pPrice'];
+
+         $target_dir    = "img/";
+         $target_file    = $target_dir.basename($_FILES['image']['name']);
+         move_uploaded_file($_FILES['image']['tmp_name'],$target_file);
+
+         $sql ="INSERT INTO products(product_name,product_price,product_image)VALUES('$p_name','$p_price','$target_file')";
+         
+         //move image to folder
+         if( mysqli_query($con,$sql)){
+            $msg ="Product Added to the database successfully!";
+         }
+         else{
+            $msg ="File not uploaded!";
+         }  
+          
+    }
+
+        
+    ?>
+                        
                         </div>
                         </div>
                         <div class="form-group mb-2">
                         <input type="submit" name="submit" class="form-control btn btn-danger btn-block" value="Add">
                         </div>
                         <div class="form-group mb-2">
-                            <h4 class="text-center"><?= $msg; ?></h4>
+                            <h5 class="text-center"><?= $msg; ?></h5>
                         </div>
                     </form>
                 </div>
--- index.php
+++ index.php
@@ -4,9 +4,78 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Product Page</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
-    <h2>Product page</h2>
+    
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h4>Rayhan Electronics</h4></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<?php
+include("connection.php");
+$sql ="SELECT * FROM products";
+$result = mysqli_query($con,$sql);
+?>
+<div class="container">
+    <div class="row">
+        <?php
+        while($row = mysqli_fetch_array($result)){
+        ?>
+        <div class="col-lg-4 mt-3 mb-3">
+            <div class="card-deck">
+                <div class="card border-info p-2">
+                    <img src="<?= $row['product_image']; ?>" height="320" alt="" class="card-img-top">
+                    <h5 class="card-title">Product : <?= $row['product_name']; ?> </h5>
+                    <h3>Price : <?= $row['product_price']; ?>/-</h3>
+                    <a href="order.php?id=<?= $row['id']; ?>" class="btn btn-danger btn-block bln-lg">Buy Now</a>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+</div>
+
+
 </body>
 </html>
+
+
+
+
+
